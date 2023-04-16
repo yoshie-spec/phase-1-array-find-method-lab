de3bc51b882f4d06d61efb528001aeea40042a28
yosselin alvarez <yoshie_9311@icloud.com>
2023-04-16 12:30:24 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    
+  ]
+
+  function superbowlWin(record) {
+    const win = record.find(year => year.result === "W");
+    if (win) {
+        return win.year;
+    } else {
+        return undefined;
+    }
+}
+
+
+
+// /it('returns a year the Denver Broncos won the superbowl', function () {
+//     expect(superbowlWin(record)).to.equal('2015');
+//       });
+    
+//       it('returns undefined if the record has no win objects', function() {
+//         const sadReality =  [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ]
+//         expect(superbowlWin(sadReality)).to.equal(undefined)
+//       });
+//     });
+// /    
